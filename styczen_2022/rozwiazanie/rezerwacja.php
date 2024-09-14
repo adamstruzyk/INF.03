2a3aecb4a045ed624ca8daf9bbdecf3dce132afe
Adam Strużyk <adamstruzyk05@gmail.com>
2024-09-14 12:06:08 +0200

    skrypt php

---
+++ styczen_2022/rozwiazanie/rezerwacja.php
@@ -0,0 +1,25 @@
+<?php
+    //nawiazanie polaczenia z baza danych
+    $conn = mysqli_connect('localhost','root','','baza');
+    if($conn == false)
+    {
+        echo "nie udało się nawiązać połączenia! <br>";
+        echo mysqli_error($conn);
+    }
+    else
+    {
+        //pobranie danych z formularza
+        $data_rezerwacji = $_POST['data'];
+        $ile_osob = $_POST['ile_osob'];
+        $nr_tel = $_POST['nr_tel'];
+        $zgadzam_sie = $_POST['zgadzam_sie'];
+
+        $polecenie = "INSERT INTO `rezerwacje`(`data_rez`, `liczba_osob`, `telefon`) VALUES ('$data_rezerwacji','$ile_osob','$nr_tel');";
+        mysqli_query($conn,$polecenie);
+
+        echo "Dodano rezerwację do bazy";
+    }
+
+    //zamkniecie polaczenia
+    mysqli_close($conn);
+?>
