08d1fcfcc6148163ec4ec306fd2c161f7289e682
Adam Strużyk <adamstruzyk05@gmail.com>
2024-09-21 11:17:13 +0200

    strony

---
+++ INF.03-03-23.01-SG/rozwiazanie/znajdz.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Kwiaty</title>
+    <link rel="stylesheet" href="styl3.css" />
+</head>
+
+<body>
+    <div id="baner">
+        <h1>Grupa Polskich Kwiaciarni</h1>
+    </div>
+    <div id="lewy">
+        <h2>Menu</h2>
+        <ol>
+            <li><a href="index.html">Strona główna</a></li>
+            <li>
+                <a href="https://www.kwiaty.pl/" target="_blank">Rozpoznaj kwiaty</a>
+            </li>
+            <li>
+                <a href="znajdz.php">Znajdź kwiaciarnię</a>
+                <ul>
+                    <li>W Warszawie</li>
+                    <li>W Malborku</li>
+                    <li>W Poznaniu</li>
+                </ul>
+            </li>
+        </ol>
+    </div>
+    <div id="prawy">
+        <h2>Znajdź kwiaciarnię</h2>
+        <form action="znajdz.php" method="post">
+            Podaj nazwę miasta: <input type="text" name="miasto">
+            <input type="submit" value="SPRAWDŹ">
+        </form>
+    </div>
+    <div id="stopka">
+        <p>Stronę opracował: Adam Strużyk 5TI</p>
+    </div>
+</body>
+
+</html>
