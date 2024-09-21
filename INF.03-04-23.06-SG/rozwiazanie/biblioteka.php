938a0c83b4d58d446ae15713627c82b78af686f5
Adam Strużyk <adamstruzyk05@gmail.com>
2024-09-21 22:39:40 +0200

    php

--- INF.03-04-23.06-SG/rozwiazanie/biblioteka.php
+++ INF.03-04-23.06-SG/rozwiazanie/biblioteka.php
@@ -13,7 +13,23 @@
     <div id="lewy">
         <h3>Polecamy dzieła autorów:</h3>
         <ol>
-            <!--skrypt 1-->
+            <?php
+                //skrypt 2 --------  polaczenie z baza
+                $conn = mysqli_connect('localhost', 'root', '', 'biblioteka');
+                
+                $zapytanie1 = "SELECT `imie`,`nazwisko` FROM `autorzy` ORDER BY `nazwisko` ASC;";
+                $wynik = mysqli_query($conn,$zapytanie1);
+                $liczba_wierszy = mysqli_num_rows($wynik);
+                for($i = 0; $i<$liczba_wierszy;$i++)
+                {
+                    $tablica = mysqli_fetch_array($wynik);
+
+                    echo "<li>".$tablica['imie']." ".$tablica['nazwisko']."</li>";
+                }
+
+                //zamkniecie polaczenia z baza
+                mysqli_close($conn);
+            ?>
         </ol>
     </div>
     <div id="srodkowy">
@@ -26,7 +42,7 @@
 
     <div id="prawy1">
             <h3>Dodaj czytelnika</h3>
-            <form action="biblioteka.php" method="POST">
+            <form method="POST">
                 imię: <input type="text" name="imie"> <br>
                 nazwisko: <input type="text" name="nazwisko"> <br>
                 symbol: <input type="number" name="symbol"> <br>
@@ -34,10 +50,29 @@
             </form>
         </div>
         <div id="prawy2">
-            <!--skrypt 2-->
-            
+            <?php
+                //skrypt 2 --------  polaczenie z baza
+                $conn = mysqli_connect('localhost', 'root', '', 'biblioteka');
+                //pobranie danych z formularza
+                @$imie = $_POST['imie'];
+                @$nazwisko = $_POST['nazwisko'];
+
+                if(isset($imie) && isset($nazwisko))
+                {
+                    //wyslanie zapytania
+                    $zapytanie2 = "INSERT INTO `czytelnicy`(`imie`, `nazwisko`) VALUES ('$imie','$nazwisko');";
+                    $wynik = mysqli_query($conn,$zapytanie2);
+                    //wypisanie tekstu
+                    echo "Czytelnik $imie $nazwisko został(a) dodany do bazy danych";
+                }
+
+                //zamkniecie polaczenia z baza
+                mysqli_close($conn);
+            ?>
         </div>
     <div id="stopka">Projekt strony: Adam Strużyk 5TI</div>
 
 </body>
+
+
 </html>
