b971da21c1db1ac55ee94a90029594c34dd5df12
Adam Strużyk <adamstruzyk05@gmail.com>
2024-09-26 11:25:28 +0200

    html

---
+++ INF.03-07-23.06-SG/rozwiazanie/biblioteka.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pl">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Biblioteka</title>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <div id="baner">
+      <h1>Biblioteka w Książkowicach Małych</h1>
+    </div>
+    <div id="lewy">
+      <h4>Dodaj czytelnika</h4>
+      <form method="post">
+        imię: <input type="text" name="imie" /> <br />
+        nazwisko: <input type="text" name="nazwisko" /> <br />
+        symbol: <input type="number" name="symbol" /> <br />
+        <input type="submit" value="AKCEPTUJ" />
+      </form>
+      <!--skrypt 1-->
+    </div>
+    <div id="srodkowy">
+      <img src="biblioteka.png" alt="biblioteka" />
+      <h6>ul.&nbsp;Czytelników&nbsp;15; Książkowice Małe</h6>
+      <a href="mailto:biuro@bib.pl">
+        <p>Czy masz jakieś uwagi?</p>
+      </a>
+    </div>
+    <div id="prawy">
+      <h4>Nasi czytelnicy:</h4>
+      <ol>
+        <!--skrypt 2-->
+      </ol>
+    </div>
+    <div id="stopka">
+      <p>Projekt witryny: Adam Strużyk 5TI</p>
+    </div>
+  </body>
+</html>
