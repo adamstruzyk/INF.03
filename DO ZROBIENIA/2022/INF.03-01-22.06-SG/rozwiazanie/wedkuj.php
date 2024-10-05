1ebaa41833afc05090712130109659c4bb02cb83
Adam Strużyk <adamstruzyk05@gmail.com>
2024-10-05 23:21:05 +0200

    php1

--- DO ZROBIENIA/2022/INF.03-01-22.06-SG/rozwiazanie/wedkuj.php
+++ DO ZROBIENIA/2022/INF.03-01-22.06-SG/rozwiazanie/wedkuj.php
@@ -13,7 +13,24 @@
     <div id="lewy1">
       <h3>Ryby zamieszkujące rzeki</h3>
       <ol>
-        <!--skrypt-->
+        <?php
+          $conn = mysqli_connect('localhost','root','','wedkowanie');
+          if(!$conn){
+            exit();
+          }
+          else{
+            $zapytanie3 = "SELECT ryby.`nazwa`, lowisko.akwen, lowisko.wojewodztwo FROM `ryby` JOIN lowisko ON lowisko.Ryby_id = ryby.id WHERE lowisko.rodzaj = 3;";
+            $wynik = mysqli_query($conn,$zapytanie3);
+            $text = '';
+            while($tablica = mysqli_fetch_array($wynik)){
+              $text .= '<li>';
+              $text .= $tablica['nazwa'].' pływa w rzece '.$tablica['akwen'].', '.$tablica['wojewodztwo'];
+              $text .= '</li>';
+            }
+            echo $text;
+          }
+          mysqli_close($conn);
+        ?>
       </ol>
     </div>
     <div id="prawy">
