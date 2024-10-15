e62a546baccb5d0385c9c45cfb1921db8044abfc
Adam Strużyk <adamstruzyk05@gmail.com>
2024-10-15 17:15:52 +0200

    css

--- DO ZROBIENIA/2024_WIOSNA/INF.03-08-24.06-SG/rozwiazanie/main.js
+++ DO ZROBIENIA/2024_WIOSNA/INF.03-08-24.06-SG/rozwiazanie/main.js
@@ -1,3 +1,20 @@
+function aktywuj(arg) {
+    document.getElementById('blok1').style.visibility = "hidden";
+    document.getElementById('blok2').style.visibility = "hidden";
+    document.getElementById('blok3').style.visibility = "hidden";
+    switch (arg){
+        case 1:
+            document.getElementById('blok1').style.visibility = "visible";
+            break;
+        case 2:
+            document.getElementById('blok2').style.visibility = "visible";
+            break;
+        case 3:
+            document.getElementById('blok3').style.visibility = "visible";
+            break;
+    }
+}
+
+
 function zwiekszPasek() {
-    alert('hello');
 }
