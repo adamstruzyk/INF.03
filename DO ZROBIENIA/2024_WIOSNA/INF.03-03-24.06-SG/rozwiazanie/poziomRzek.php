35ae364784fc9bed93eea85c4a3031e719958474
Adam Strużyk <adamstruzyk05@gmail.com>
2024-10-18 19:53:33 +0200

    html

---
+++ DO ZROBIENIA/2024_WIOSNA/INF.03-03-24.06-SG/rozwiazanie/poziomRzek.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="pl">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Poziomy rzek</title>
+    <link rel="stylesheet" href="styl.css" />
+  </head>
+  <body>
+    <header id="baner1">
+      <img src="obraz1.png" alt="Mapa Polski" />
+    </header>
+    <header id="baner2">
+      <h1>Rzeki w województwie dolnośląskim</h1>
+    </header>
+    <menu id="menu">
+      <form method="post">
+        <input type="radio" name="opcje" /> Wszystkie
+        <input type="radio" name="opcje" /> Ponad stan ostrzegawczy
+        <input type="radio" name="opcje" /> Ponad stan alarmowy
+        <input type="submit" value="Pokaż" />
+      </form>
+    </menu>
+    <section id="lewy">
+      <h3>Stany na dzień 2022-05-05</h3>
+      <table>
+        <tr>
+          <th>Wodomierz</th>
+          <th>Rzeka</th>
+          <th>Ostrzegawczy</th>
+          <th>Alarmowy</th>
+          <th>Aktualny</th>
+        </tr>
+        <!--skrypt 1-->
+      </table>
+    </section>
+    <section id="prawy">
+      <h3>Informacje</h3>
+      <ul>
+        <li>Brak ostrzeżeń o burzach z gradem</li>
+        <li>Smog w mieście Wrocław</li>
+        <li>Silny wiatr w Karkonoszach</li>
+      </ul>
+      <h3>Średnie stany wód</h3>
+      <!--skrypt 2-->
+      <a href="https://komunikaty.pl">Dowiedz się więcej</a>
+      <img src="obraz2.jpg" alt="rzeka" />
+    </section>
+    <footer id="stopka">
+      <p>Stronę wykonał: Adam Strużyk 5TI</p>
+    </footer>
+  </body>
+</html>
