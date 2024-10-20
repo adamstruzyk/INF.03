d7fefdfa112adb2f9f8471cd2713cef0c33fdb61
Adam Strużyk <adamstruzyk05@gmail.com>
2024-10-20 12:43:02 +0200

    php

--- DO ZROBIENIA/2024_WIOSNA/INF.03-05-24.06-SG/rozwiazanie/KupAuto.php
+++ DO ZROBIENIA/2024_WIOSNA/INF.03-05-24.06-SG/rozwiazanie/KupAuto.php
@@ -59,11 +59,48 @@
       <h2>Wybierz markę</h2>
       <form method="post">
         <select name="marka" id="wybierz_marke">
-          <!--skrypt 3-->
+          <?php
+            $conn = mysqli_connect('localhost','root','','kupauto');
+            if(!$conn){
+              exit();
+            }
+            else{
+              $zapytanie1 = "SELECT `nazwa` FROM `marki`;";
+              $wynik = mysqli_query($conn,$zapytanie1);
+              $text = "";
+              while($tab = mysqli_fetch_array($wynik)){
+                $text .= "<option value='$tab[0]'>$tab[0]</option>";
+              }
+              echo $text;
+            }
+            mysqli_close($conn);
+          ?>
         </select>
-        <input type="submit" value="Wyszukaj" />
+        <button type='submit'>Wyszukaj</button>
       </form>
-      <!--skrypt 4-->
+      <?php
+        $conn = mysqli_connect('localhost','root','','kupauto');
+        if(!$conn){
+          exit();
+        }
+        else{
+          @$marka = $_POST['marka'];
+          if(isset($marka)){
+            $zapytanie4 = "SELECT marki.nazwa, samochody.model, samochody.cena, samochody.zdjecie FROM `samochody` JOIN marki ON marki.id = samochody.marki_id WHERE marki.nazwa = '$marka';";
+            $wynik = mysqli_query($conn,$zapytanie4);
+            $text = "";
+            while($tab = mysqli_fetch_array($wynik)){
+              $text .= "<section class='blok_generowany'>";
+              $text .= "<img src='$tab[3]' alt='$tab[1]'>";
+              $text .= "<h4>$tab[0] $tab[1]</h4>";
+              $text .= "<h4>Cena: $tab[2]</h4>";
+              $text .= "</section>";
+            }
+            echo $text;
+          }
+        }
+        mysqli_close($conn);
+      ?>
     </section>
     <footer id="stopka">
       <p>Stronę wykonał: Adam Strużyk 5TI</p>
@@ -74,24 +111,3 @@
   </body>
 </html>
 
-<?php
-  $conn = mysqli_connect('localhost','root','','kupauto');
-  if(!$conn){
-    exit();
-  }
-  else{
-    $zapytanie1 = "SELECT `nazwa` FROM `marki`;";
-    $wynik = mysqli_query($conn,$zapytanie1);
-    $text = "";
-    while($tab = mysqli_fetch_array($wynik)){
-      $text .= "<section class='blok_generowany'>";
-      $text .= "<img src='$tab[4]' alt='$tab[1]'>";
-      $text .= "<h4>$tab[0] $tab[1]</h4>";
-      $text .= "<p>Rocznik: $tab[2]</p>";
-      $text .= "<h4>Cena: $tab[3]</h4>";
-      $text .= "</section>";
-    }
-    echo $text;
-  }
-  mysqli_close($conn);
-?>
