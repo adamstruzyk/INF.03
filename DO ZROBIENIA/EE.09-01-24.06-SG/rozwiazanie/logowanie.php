1d847f0510c03c63c0f09b940bd619bd0ce12f9d
Adam Strużyk <adamstruzyk05@gmail.com>
2024-11-06 18:50:37 +0100

    php

--- DO ZROBIENIA/EE.09-01-24.06-SG/rozwiazanie/logowanie.php
+++ DO ZROBIENIA/EE.09-01-24.06-SG/rozwiazanie/logowanie.php
@@ -21,7 +21,44 @@
         powtórz hasło: <input type="password" name="haslo2" /> <br />
         <input type="submit" value="Zapisz" />
       </form>
-      <!--efekt dzialania skryptu-->
+      <?php
+        $conn = mysqli_connect('localhost','root','', 'psy');
+        if(!$conn){
+          exit();
+        }
+        else{
+          @$login = $_POST['login'];
+          @$haslo1 = $_POST['haslo1'];
+          @$haslo2 = $_POST['haslo2'];
+          if(isset($login) && isset($haslo1) && isset($haslo2)){
+            if(empty($login) || empty($haslo1) || empty($haslo2)){
+              echo "<p>wypełnij wszystkie pola</p>";
+            }
+            else{
+              $zapytanie3 = "SELECT `login` FROM `uzytkownicy`;";
+              $wynik3 = mysqli_query($conn, $zapytanie3);
+              $czy_login_wystepuje = false;
+              $hasla_takie_same = false;
+              while($tab = mysqli_fetch_array($wynik3)){
+                if($tab[0]==$login){
+                  $czy_login_wystepuje=true;
+                  echo "<p>login występuje w bazie danych, konto nie zostało dodane</p>";
+                }
+              }
+              if(!$czy_login_wystepuje && $haslo1 != $haslo2){
+                echo "<p>hasła nie są takie same, konto nie zostało dodane</p>";
+              }
+              else if(!$czy_login_wystepuje && $haslo1 == $haslo2){
+                $szyfr = sha1($haslo1, false);
+                $zapytanie2 = "INSERT INTO `uzytkownicy`(`login`, `haslo`) VALUES ('$login','$szyfr');";
+                mysqli_query($conn,$zapytanie2);
+                echo "<p>Konto zostało dodane</p>";
+              }
+            }
+          }
+        }
+        mysqli_close($conn);
+      ?>
     </section>
     <section id="prawy2">
       <h2>Zapraszamy wszystkich</h2>
