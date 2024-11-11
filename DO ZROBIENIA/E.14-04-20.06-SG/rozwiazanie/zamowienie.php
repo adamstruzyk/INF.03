b280b6878459c4fcf850a0a2abc260aed054e73b
Adam Strużyk <adamstruzyk05@gmail.com>
2024-11-11 10:09:42 +0100

    galeria.html

---
+++ DO ZROBIENIA/E.14-04-20.06-SG/rozwiazanie/zamowienie.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pl">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Sklep</title>
+    <link rel="stylesheet" href="styl.css" />
+  </head>
+  <body>
+    <header id="baner">
+      <h1>Ozdoby - sklep</h1>
+    </header>
+    <section id="lewy">
+      <h2>Ozdoby</h2>
+      <a href="galeria.html">Galeria</a>
+      <a href="zamowienie.php">Zamówienie</a>
+    </section>
+    <section id="srodkowy"></section>
+    <section id="prawy">
+      <img src="animacja.gif" />
+    </section>
+    <footer id="stopka">
+      <h3>Autor strony: Adam Strużyk 5TI</h3>
+    </footer>
+  </body>
+</html>
