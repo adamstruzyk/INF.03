73099c82c4bb1e3be149a8f450184778abd1540d
Adam Strużyk <adamstruzyk05@gmail.com>
2024-12-03 19:49:42 +0100

    html

---
+++ EE.09/E.14-02-18.01/rozwiazanie/portal.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="pl">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Ogłoszenia drobne</title>
+    <link rel="stylesheet" href="styl2.css" />
+  </head>
+  <body>
+    <header id="baner">
+      <h2>Ogłoszenia drobne</h2>
+    </header>
+    <section id="lewy">
+      <h2>Ogłoszeniodawcy</h2>
+      <!--skrypt polaczenia z baza-->
+    </section>
+    <section id="prawy">
+      <h2>Nasze kategorie</h2>
+      <ul>
+        <li>Książki</li>
+        <li>Muzyka</li>
+        <li>Multimedia</li>
+      </ul>
+      <img src="ksiazki.jpg" alt="uwolnij swoją książkę" />
+      <table>
+        <tr>
+          <td>Ile?</td>
+          <td>Koszt</td>
+          <td>Promocja</td>
+        </tr>
+        <tr>
+          <td>1-40</td>
+          <td>1,20 PLN</td>
+          <td rowspan="2">
+            Subskrybuj newsletter upust 0,30 PLN na ogłoszenie
+          </td>
+        </tr>
+        <tr>
+          <td>41 i więcej</td>
+          <td>0,70 PLN</td>
+          <td></td>
+        </tr>
+      </table>
+    </section>
+    <section id="stopka">
+      Portal ogłoszenia drobne opracował: Adam Strużyk 5TI
+    </section>
+  </body>
+</html>
