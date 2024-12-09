dd100fb9dc5ae8bad64833699e78b658dc3ac413
Adam Strużyk <adamstruzyk05@gmail.com>
2024-12-09 09:52:39 +0100

    php

---
+++ EE.09/EE.09-03-22.06-SG/rozwiazanie/opinie.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pl">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Opinie klientów</title>
+    <link rel="stylesheet" href="styl3.css" />
+  </head>
+  <body>
+    <header id="baner">
+      <h1>Hurtownia spożywcza</h1>
+    </header>
+    <main id="glowny">
+      <h2>Opinie naszych klientów</h2>
+      <!--skrypt 1-->
+    </main>
+    <footer id="stopka1">
+      <h3>Współpracują z nami</h3>
+      <a href="http://sklep.pl/">Sklep 1</a>
+    </footer>
+    <footer id="stopka2">
+      <h3>Nasi top klienci</h3>
+      <ol>
+        <!--skrypt 2-->
+      </ol>
+    </footer>
+    <footer id="stopka3">
+      <h3>Skontaktuj się</h3>
+      <p>telefon: 111222333</p>
+    </footer>
+    <footer id="stopka4">
+      <h3>Autor: Adam Strużyk 5TI</h3>
+    </footer>
+  </body>
+</html>
