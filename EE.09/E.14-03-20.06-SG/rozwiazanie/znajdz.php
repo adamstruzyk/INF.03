1fd913ab4741173a04bd96512ec942c8b083fb22
Adam Strużyk <adamstruzyk05@gmail.com>
2024-12-19 19:07:59 +0100

    php

--- EE.09/E.14-03-20.06-SG/rozwiazanie/znajdz.php
+++ EE.09/E.14-03-20.06-SG/rozwiazanie/znajdz.php
@@ -22,8 +22,24 @@
         <img src="roza.jpg" alt="Róża" />
         <p>Podaj miejscowość, w której poszukujesz kwiaciarni:</p>
         <form method="post">
-            <input type="text" name="miejscowość"> <button type='submit'>SPRAWDŹ</button>
+            <input type="text" name="miejscowosc"> <button type='submit'>SPRAWDŹ</button>
         </form>
+        <?php
+            $conn = mysqli_connect('localhost','root','','kwiaciarnia');
+            if(!$conn){
+                exit();
+            }else{
+                @$miejscowosc = $_POST['miejscowosc'];
+                if(isset($miejscowosc)){
+                    $zapytanie1 = "SELECT `nazwa`,`ulica` FROM `kwiaciarnie` WHERE `miasto` like '$miejscowosc';";
+                    $wynik = mysqli_query($conn, $zapytanie1);
+                    $row = mysqli_fetch_array($wynik);
+
+                    echo "$row[0], $row[1]";
+                }
+            }
+            mysqli_close($conn);
+        ?>
       </section>
     </main>
     <footer id="stopka">
