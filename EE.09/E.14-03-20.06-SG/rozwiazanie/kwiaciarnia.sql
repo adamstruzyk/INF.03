5e8fcfebccd379f1df5e420afbd620730a076df2
Adam Strużyk <adamstruzyk05@gmail.com>
2024-12-19 18:21:11 +0100

    baza

---
+++ EE.09/E.14-03-20.06-SG/rozwiazanie/kwiaciarnia.sql
@@ -0,0 +1,106 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 19, 2024 at 06:20 PM
+-- Wersja serwera: 10.4.32-MariaDB
+-- Wersja PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `kwiaciarnia`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `kwiaciarnie`
+--
+
+CREATE TABLE `kwiaciarnie` (
+  `id_kwiaciarni` int(11) NOT NULL,
+  `nazwa` text DEFAULT NULL,
+  `miasto` text DEFAULT NULL,
+  `ulica` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `kwiaciarnie`
+--
+
+INSERT INTO `kwiaciarnie` (`id_kwiaciarni`, `nazwa`, `miasto`, `ulica`) VALUES
+(1, 'Astra', 'Warszawa', 'Sobieskiego'),
+(2, 'Melisa', 'Malbork', 'Konopnickiej');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `zamowienia`
+--
+
+CREATE TABLE `zamowienia` (
+  `id_zam` int(11) NOT NULL,
+  `id_kwiaciarni` int(11) DEFAULT NULL,
+  `data` text DEFAULT NULL,
+  `kwiaty` text DEFAULT NULL,
+  `cena` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `zamowienia`
+--
+
+INSERT INTO `zamowienia` (`id_zam`, `id_kwiaciarni`, `data`, `kwiaty`, `cena`) VALUES
+(1, 1, '16.01.2017', 'róża', 250),
+(2, 1, '10.01.2017', 'gerbera', 120),
+(3, 2, '06.01.2017', 'róża', 200),
+(4, 2, '09.01.2017', 'goździk', 170),
+(5, 1, '07.01.2017', 'róża', 125),
+(6, 1, '11.01.2017', 'róża', 75);
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `kwiaciarnie`
+--
+ALTER TABLE `kwiaciarnie`
+  ADD PRIMARY KEY (`id_kwiaciarni`);
+
+--
+-- Indeksy dla tabeli `zamowienia`
+--
+ALTER TABLE `zamowienia`
+  ADD PRIMARY KEY (`id_zam`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `kwiaciarnie`
+--
+ALTER TABLE `kwiaciarnie`
+  MODIFY `id_kwiaciarni` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `zamowienia`
+--
+ALTER TABLE `zamowienia`
+  MODIFY `id_zam` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
