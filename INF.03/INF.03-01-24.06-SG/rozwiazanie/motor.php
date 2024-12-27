c25d6a0fa5d30ade7b65835a7801f6f812317629
Adam Strużyk <adamstruzyk05@gmail.com>
2024-12-27 19:07:19 +0100

    rozwiazanie

--- INF.03/INF.03-01-24.06-SG/rozwiazanie/motor.php
+++ INF.03/INF.03-01-24.06-SG/rozwiazanie/motor.php
@@ -6,72 +6,66 @@
     <title>Motocykle</title>
     <link rel="stylesheet" href="styl.css" />
   </head>
-  <div id="tlo">
-    <img src="motor.png" alt="motocykl" />
-  </div>
   <body>
+    <img src="motor.png" alt="motocykl" id="motocykl" />
+
     <header id="baner">
       <h1>Motocykle - moja pasja</h1>
     </header>
-    <main id="lewy">
-      <h2>Gdzie pojechać?</h2>
-      <dl>
-        <?php
-          $conn = mysqli_connect('localhost','root','','motory');
-          if(!$conn){
-            exit();
-          }
-          else{
-            $zapytanie2 = "SELECT wycieczki.nazwa, wycieczki.opis, wycieczki.poczatek, zdjecia.zrodlo FROM `wycieczki` JOIN zdjecia ON wycieczki.zdjecia_id = zdjecia.id;";
-            $wynik = mysqli_query($conn,$zapytanie2);
-            $text = "";
-
-            while($tablica = mysqli_fetch_array($wynik)){
-              $text .= '<dt>'.$tablica['nazwa'].' rozpoczyna się w '.$tablica['poczatek'].' <a href="'.$tablica['zrodlo'].'">zobacz zdjęcie</a></dt>';
-              $text .= '<dd>'.$tablica['opis'].'</dd>';
-            }
-            echo $text;
-          }
-          mysqli_close($conn);
-        ?>
-      </dl>
-    </main>
-    <aside id="prawy1">
-      <h2>Co kupić?</h2>
-      <ol>
-        <li>Honda CBR125R</li>
-        <li>Yamaha YBR125</li>
-        <li>Honda VFR800i</li>
-        <li>Honda CBR1100XX</li>
-        <li>BMW R1200GS LC</li>
-      </ol>
-    </aside>
-    <aside id="prawy2">
-      <h2>Statystyki</h2>
-      <p>Wpisanych wycieczek:
-        <?php
+    <main>
+      <article id="lewy">
+        <h2>Gdzie pojechać?</h2>
+        <dl>
+          <?php
             $conn = mysqli_connect('localhost','root','','motory');
             if(!$conn){
               exit();
-            }
-            else{
-              $zapytanie3 = "SELECT COUNT(*) AS `ile` FROM `wycieczki`;";
-              $wynik = mysqli_query($conn,$zapytanie3);
-              $tablica = mysqli_fetch_array($wynik);
-              echo $tablica['ile'];
+            }else{
+              $wynik = mysqli_query($conn, "SELECT wycieczki.nazwa, wycieczki.opis, wycieczki.poczatek, zdjecia.zrodlo FROM `wycieczki` JOIN zdjecia ON zdjecia.id = wycieczki.zdjecia_id;");
+              while($row = mysqli_fetch_array($wynik)){
+                echo "<dt>$row[0], rozpoczyna się w $row[2], <a href='$row[3].jpg'>zobacz zdjęcie</a></dt>";
+                echo "<dd>$row[1]</dd>";
+              }
             }
             mysqli_close($conn);
           ?>
-      </p>
-
-      <p>Użytkowników forum: 200</p>
-      <p>Przesłanych zdjęć: 1300</p>
-    </aside>
-    <footer id="stopka">Stronę wykonał: Adam Strużyk 5TI</footer>
+        </dl>
+      </article>
+      <aside>
+        <section id="prawy1">
+          <h2>Co kupić?</h2>
+          <ol>
+            <li>Honda CBR125R</li>
+            <li>Yamaha YBR125</li>
+            <li>Honda VFR800i</li>
+            <li>Honda CBR1100XX</li>
+            <li>BMW R1200GS LC</li>
+          </ol>
+        </section>
+        <section id="prawy2">
+          <h2>Statystyki</h2>
+          <p>Wpisanych wycieczek: 
+            <?php
+              $conn = mysqli_connect('localhost','root','','motory');
+              if(!$conn){
+                exit();
+              }else{
+                $wynik = mysqli_query($conn, "SELECT COUNT(*) FROM `wycieczki`;");
+                while($row = mysqli_fetch_array($wynik)){
+                  echo "<p>Wpisanych wycieczek: $row[0]</p>";
+                }
+              }
+              mysqli_close($conn);
+            ?>
+          </p>
+          <p>użytkowników forum: 200</p>
+          <p>Przesłanych zdjęć: 1300</p>
+        </section>
+      </aside>
+    </main>
+    <footer id="stopka">
+      <p>Stronę wykonał: Adam Strużyk 5TI</p>
+    </footer>
   </body>
-  
 </html>
-
-
-
 
