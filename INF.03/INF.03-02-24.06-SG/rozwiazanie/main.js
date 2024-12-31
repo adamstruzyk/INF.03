9c61234df5e3b7e48f764d5e75c9315e0d6115f9
Adam Strużyk <adamstruzyk05@gmail.com>
2024-12-31 22:36:07 +0100

    js

--- INF.03/INF.03-02-24.06-SG/rozwiazanie/main.js
+++ INF.03/INF.03-02-24.06-SG/rozwiazanie/main.js
@@ -1,31 +1,32 @@
-let wypowiedzi_krzysztofa = [
-    "Świetnie!",
-    "Kto gra główną rolę?",
-    "Lubisz filmy Tego reżysera?",
-    "Będę 10 minut wcześniej",
-    "Może kupimy sobie popcorn?",
-    "Ja wolę Colę",
-    "Zaproszę jeszcze Grześka",
-    "Tydzień temu też byłem w kinie na Diunie",
-    "Ja funduję bilety"];
-
+wypowiedzi = ["Świetnie!",
+"Kto gra główną rolę?",
+"Lubisz filmy Tego reżysera?",
+"Będę 10 minut wcześniej",
+"Może kupimy sobie popcorn?",
+"Ja wolę Colę",
+"Zaproszę jeszcze Grześka",
+"Tydzień temu też byłem w kinie na Diunie",
+"Ja funduję bilety"];
 
 function wyslij() {
-    let wiadomosc = document.getElementById("wiadomosc").value;
-    let zawartosc_chatu = document.getElementById("chat").innerHTML;
-    let wypowiedz_jolanty = '<div class="jolka"><img src="Jolka.jpg" /><p>' + wiadomosc + '</p></div>';
-    zawartosc_chatu = zawartosc_chatu + wypowiedz_jolanty;
-    document.getElementById("chat").innerHTML = zawartosc_chatu;
-    //aktualna pozycja scrolla wzgledem góry (zwraca zero pikseli na samej gorze) = maksymalna wysokosc scrolla (zwraca max liczbe pikseli)
-    document.getElementById("chat").scrollTop = document.getElementById("chat").scrollHeight;
+    let chat = document.getElementById('blok_chatu');
+    let tresc = document.getElementById('wiadomosc').value;
+
+    let wiadomosc = document.createElement("section");
+    wiadomosc.classList.add("jolka");
+    wiadomosc.innerHTML = "<img src='Jolka.jpg'><p>" + tresc + "</p>";
+    chat.appendChild(wiadomosc);
+    wiadomosc.scrollIntoView();
 }
 
-function los() {
-    let losowa = Math.floor(Math.random() * 9);
-    let wiadomosc = wypowiedzi_krzysztofa[losowa];
-    let zawartosc_chatu = document.getElementById("chat").innerHTML;
-    let wypowiedz_krzysztofa = '<div class="krzysztof"><img src="Krzysiek.jpg" /><p>' + wiadomosc + '</p></div>';
-    zawartosc_chatu = zawartosc_chatu + wypowiedz_krzysztofa;
-    document.getElementById("chat").innerHTML = zawartosc_chatu;
-    document.getElementById("chat").scrollTop = document.getElementById("chat").scrollHeight;
+function losuj() {
+    let los = Math.floor(Math.random() * 9);
+    let chat = document.getElementById('blok_chatu');
+    let tresc = wypowiedzi[los];
+
+    let wiadomosc = document.createElement("section");
+    wiadomosc.classList.add("krzysiek");
+    wiadomosc.innerHTML = "<img src='Krzysiek.jpg'><p>" + tresc + "</p>";
+    chat.appendChild(wiadomosc);
+    wiadomosc.scrollIntoView();
 }
