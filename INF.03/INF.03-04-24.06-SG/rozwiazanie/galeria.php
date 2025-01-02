bdff4a7be1e240aad6480de92fa3ce9350a5a895
Adam Strużyk <adamstruzyk05@gmail.com>
2025-01-02 19:55:47 +0100

    html

--- INF.03/INF.03-04-24.06-SG/rozwiazanie/galeria.php
+++ INF.03/INF.03-04-24.06-SG/rozwiazanie/galeria.php
@@ -1,78 +1,71 @@
 <!DOCTYPE html>
 <html lang="pl">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Galeria</title>
-    <link rel="stylesheet" href="styl.css" />
-  </head>
-  <body>
+    <link rel="stylesheet" href="styl.css">
+</head>
+<body>
     <header id="baner">
-      <h1>Zdjęcia</h1>
+        <h1>Zdjęcia</h1>
     </header>
-    <section id="lewy">
-      <h2>Tematy zdjęć</h2>
-      <ol>
-        <li>Zwierzęta</li>
-        <li>Krajobrazy</li>
-        <li>Miasta</li>
-        <li>Przyroda</li>
-        <li>Samochody</li>
-      </ol>
-    </section>
-    <section id="srodkowy">
-      <?php
-        $conn = mysqli_connect('localhost','root','','galeria');
-        if(!$conn){
-          exit();
-        }
-        else{
-          $zapytanie2 = "SELECT zdjecia.plik, zdjecia.tytul, zdjecia.polubienia, autorzy.imie, autorzy.nazwisko FROM `zdjecia` JOIN autorzy ON autorzy.id = zdjecia.autorzy_id ORDER BY autorzy.nazwisko ASC;";
-          $wynik = mysqli_query($conn,$zapytanie2);
-          $text = '';
-          while($tab = mysqli_fetch_array($wynik)){
-            $text .= '<section class="zdjecia"">';
-            $text .= '<img src="'.$tab['plik'].'" alt="zdjęcie">';
-            $text .= '<h3>'.$tab['tytul'].'</h3>';
-            if($tab['polubienia']>40){
-              $text .= '<p>Autor: '.$tab['imie'].' '.$tab['nazwisko'].'.<br> Wiele osób polubiło ten obraz</p>';
-            }
-            else{
-              $text .= '<p>Autor: '.$tab['imie'].' '.$tab['nazwisko'].'</p>';
-            }
-            $text .= '<a href="'.$tab['plik'].'" download>Pobierz</a>';
-            $text .= '</section>';
-          }
+    <aside id="lewy">
+        <h2>Tematy zdjęć</h2>
+        <ol>
+            <li>Zwierzęta</li>
+            <li>Krajobrazy</li>
+            <li>Miasta</li>
+            <li>Przyroda</li>
+            <li>Samochody</li>
+        </ol>
+    </aside>
+    <main>
+        <article id="srodkowy">
+        <?php
+            $conn = mysqli_connect('localhost', 'root', '', 'galeria');
+            if(!$conn){
+                exit();
+            }else{
+                $wynik = mysqli_query($conn, "SELECT zdjecia.plik, zdjecia.tytul, zdjecia.polubienia, autorzy.imie, autorzy.nazwisko FROM `zdjecia` JOIN autorzy ON autorzy.id = zdjecia.autorzy_id ORDER BY autorzy.nazwisko ASC;");
 
-          echo $text;
-        }
-        mysqli_close($conn);
-      ?>
-    </section>
-    <section id="prawy">
-      <h2>Najbardziej lubiane</h2>
-      <?php
-        $conn = mysqli_connect('localhost','root','','galeria');
-        if(!$conn){
-          exit();
-        }
-        else{
-          $zapytanie1 = "SELECT `tytul`,`plik` FROM `zdjecia` WHERE `polubienia` >= 100;";
-          $wynik = mysqli_query($conn,$zapytanie1);
-          $text = '';
-          while($tab = mysqli_fetch_array($wynik)){
-            $text .= '<img src="'.$tab['plik'].'" alt="'.$tab['tytul'].'">';
-          }
-          echo $text;
-        }
-        mysqli_close($conn);
-      ?>
-      <strong>Zobacz wszystkie nasze zdjęcia</strong>
-    </section>
+                while($row = mysqli_fetch_array($wynik)){
+                    echo "<section class='blok'>";
+                    echo "<img src='$row[0]' alt='zdjęcie'>";
+                    echo "<h3>$row[1]</h3>";
+                    if($row[2] > 40){
+                        echo "<p>Autor: $row[3] $row[4]. Wiele osób polubiło ten obraz. <br></p>";
+                    }else{
+                        echo "<p>Autor: $row[3] $row[4].<br></p>";
+                    }
+                    echo "<a href='$row[0]' download>Pobierz</a>";
+                    echo "</section>";
+                }
+            }
+            mysqli_close($conn);
+        ?>
+        </article>
+    </main>
+    <aside id="prawy">
+        <h2>Najbardziej lubiane</h2>
+        <?php
+            $conn = mysqli_connect('localhost', 'root', '', 'galeria');
+            if(!$conn){
+                exit();
+            }else{
+                $wynik = mysqli_query($conn, "SELECT `tytul`,`plik` FROM `zdjecia` WHERE `polubienia` >= 100;");
+
+                while($row = mysqli_fetch_array($wynik)){
+                    echo "<img src='$row[1]' alt='$row[0]'>";
+                }
+            }
+            mysqli_close($conn);
+        ?>
+        <strong>Zobacz wszystkie nasze Zdjęcia </strong>
+    </aside>
     <footer id="stopka">
-      <h5>Stronę wykonał: Adam Strużyk 5TI</h5>
+        <h5>Stronę wykonał: Adam Strużyk 5TI</h5>
     </footer>
-  </body>
+</body>
 </html>
-
 
